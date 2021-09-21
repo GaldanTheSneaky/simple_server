f61aed74fe1b630bc892f475d56b70730cb17535
Даниил Жуков <avangerslate@yandex.ru>
2021-09-21 13:48:42 +0800

    works fine

--- server.py
+++ server.py
@@ -10,78 +10,33 @@ class Server:
         self._addr = (self._host, self._port)
         self._format = format
         self._disconnect_message = disconnect_message
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(self._addr)
+        self._server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server.bind(self._addr)
 
     def handle_client(self, conn, addr):
         print(f"[NEW CONNECTION] {addr} connected.")
 
         connected = True
         while connected:
-            msg_length = conn.recv(HEADER).decode(FORMAT)
+            msg_length = conn.recv(self._header).decode(self._format)
             if msg_length:
                 msg_length = int(msg_length)
-                msg = conn.recv(msg_length).decode(FORMAT)
-                if msg == DISCONNECT_MESSAGE:
+                msg = conn.recv(msg_length).decode(self._format)
+                if msg == self._disconnect_message:
                     connected = False
 
                 print(f"[{addr}] {msg}")
-                conn.send("Msg received".encode(FORMAT))
+                conn.send(f"Msg received '{msg}'".encode(self._format))
 
         conn.close()
 
     def run(self):
-        server.listen()
-        print(f"[LISTENING] Server is listening on {SERVER}")
+        self._server.listen()
+        print(f"[LISTENING] Server is listening on {self._host}")
         while True:
-            conn, addr = server.accept()
-            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            conn, addr = self._server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
             thread.start()
             print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
-
-
-
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-
-
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
-
-    conn.close()
-
-
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-
-
-print("[STARTING] server is starting...")
-start()
